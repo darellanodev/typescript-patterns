60aef8190c442c03f1012888adc83da5221f4b99
darellanodev <darellano.aliaga@gmail.com>
2025-02-06 20:50:18 +0100

    Add child to a MenuItem and get the child count (#10)

--- src/menuItem.test.ts
+++ src/menuItem.test.ts
@@ -6,4 +6,10 @@ describe('MenuItem', () => {
     const menuItem = new MenuItem('File')
     expect(menuItem.name).toBe('File')
   })
+  it('should add items and count them', () => {
+    const fileMenu = new MenuItem('File')
+    fileMenu.addChild(new MenuItem('Open'))
+    fileMenu.addChild(new MenuItem('New'))
+    expect(fileMenu.getChildCount()).toBe(2)
+  })
 })
--- src/menuItem.ts
+++ src/menuItem.ts
@@ -1,6 +1,13 @@
 export class MenuItem {
+  #children: MenuItem[] = []
   name: string
   constructor(name: string) {
     this.name = name
+  }
+  addChild(child: MenuItem) {
+    this.#children.push(child)
+  }
+  getChildCount(): number {
+    return this.#children.length
   }
 }
